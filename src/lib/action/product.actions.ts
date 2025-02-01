4e25e09a7515c0d82f591d1df9ce1fc4ee08fdea
zachhh14 <zactec115@gmail.com>
2025-02-01 17:19:58 +0800

    chore: refactor src directories

---
+++ src/lib/action/product.actions.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { Car } from '@/types/car'
+import { createClient } from '@/supabase/server'
+
+export const addProduct = async (car: Car) => {
+    const supabase = await createClient()
+
+    try {
+        const { data, error } = await supabase
+            .from('cars')
+            .insert({ ...car })
+            .select()
+            .single()
+
+        if (error) throw error
+
+        return { data, error: null }
+    } catch (error) {
+        console.error('Error listing:', error)
+
+        return { data: null, error }
+    }
+}
